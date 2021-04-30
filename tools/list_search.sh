6e66bc798c86fb8d039213b9a4613115bddb7b8c
Edargorter <zdbowditch777@gmail.com>
2021-04-30 15:54:43 +0200

    List search 'lgrep'!

---
+++ tools/list_search.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+listfile=$1
+searchfile=$2
+if [ $# -eq 0 ]
+then
+	echo -e "Run with:\n lgrep list_file search_file"
+	exit 1
+elif [ $# -eq 1 ]
+then
+	echo -e "Please provide path to search file."
+	exit 2
+fi
+
+for item in $listfile; 
+do
+	grep -ir "$item" $searchfile
+done
