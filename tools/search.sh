f0a5f83c645b9f6b587ad5905f7afdbcdd31aa20
Edargorter <zdbowditch777@gmail.com>
2021-06-08 12:47:29 +0200

    searching tool

--- tools/search.sh
+++ tools/search.sh
@@ -29,16 +29,23 @@ fi
 
 wcl=$(wc -l "$filename" | cut -d " " -f 1)
 words=$(cat $wordlist)
+wlwc=$(wc -l $wordlist | cut -d " " -f 1)
 echo "Lines: $wcl" 
 index=0
 prog=0
+prev_size=0
+lines=0
 
 for st in $words
 do
+	echo -n "Word: $st"
 	grep -ir $st $filename >> $outfile 
-	echo "Progress: $prog %"
+	curr=$(wc -l $outfile | cut -d " " -f 1)
+	lines=$(( $curr - $prev_size ))
+	echo -en " \t\t --- Lines: $lines --- Progress: $prog%\n"
+	prev_size=$curr
 	index=$(($index+1))
-	prog=$(expr $((100 * $index)) / $wcl)
+	prog=$(expr $((100 * $index)) / $wlwc)
 done
 
-echo "Done."
+echo "Done. See results in $outfile."
